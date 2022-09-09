04216a20175ea40f6cd3b522773093476f7d72bd
AntonioPortada <antonio_portada25@hotmail.com>
2022-09-09 09:46:05 -0500

    cargar de proyecto

---
+++ src/main/java/com/antonio/correspondenciaback/CorrespondenciaBackApplication.java
@@ -0,0 +1,13 @@
+package com.antonio.correspondenciaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrespondenciaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrespondenciaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antonio/correspondenciaback/CorrespondenciaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonio.correspondenciaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrespondenciaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
